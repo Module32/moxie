0c61a2511d84a94d0e01d19a12c56cdbdef2e6da
Module32 <78182297+Module32@users.noreply.github.com>
2022-03-27 19:28:02 -0400

    dam

--- whm/components/footer.js
+++ whm/components/footer.js
@@ -13,7 +13,7 @@ export default function Layout({ children, home }) {
       <footer>
         <div style={{display: 'flex'}}>
           <div style={{flexDirection: 'row'}}><p>Made with 🍫 by Anaya and Ekya</p></div>
-          <div style={{flexDirection: 'row', marginLeft: 'auto'}}><p><a href="https://discord.gg/Gv6zCtSJCu">Join MOXIE!</a></p></div>
+          <div style={{flexDirection: 'row', marginLeft: 'auto'}}><p><a href="https://discord.gg/Gv6zCtSJCu" className="footer">Join MOXIE!</a></p></div>
         </div>
       </footer>
     </div>
